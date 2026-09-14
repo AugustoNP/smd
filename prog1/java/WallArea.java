04bf4f34607065a2a7f0a04fb86c29580d769509
Augusto Penha <augustonpenha@gmail.com>
2026-09-14 15:14:53 -0300

    Finished WallScanner

---
+++ prog1/java/WallArea.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class WallArea{
+
+	public static void main (String[] args){
+		Scanner inputScanner = new Scanner(System.in);
+
+		System.out.println("Por favor, escreva as medidas de sua parede em METROS");
+        
+		System.out.println("\nEscreva a altura de sua parede!");
+		float height = inputScanner.nextFloat();
+		
+		System.out.println("Escreva a largura de sua parede!");
+		float width = inputScanner.nextFloat();
+		
+		float result = CalculateWallArea(width, height);
+		System.out.println("\nA área de sua parede é : " + result + " METROS");
+		}
+ 
+	static float CalculateWallArea(float width, float height){
+		float result = width * height;
+		return result;
+	}
+}
