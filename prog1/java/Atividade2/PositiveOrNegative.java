d71c5259de27fe1194dd74002fbf129d36f32cce
Augusto Penha <augustonpenha@gmail.com>
2026-09-22 07:45:29 -0300

    Changed file organization, started first file of second activity

---
+++ prog1/java/Atividade2/PositiveOrNegative.java
@@ -0,0 +1,33 @@
+package Atividade2;
+
+import javax.swing.JOptionPane;
+
+public class PositiveOrNegative{
+
+
+
+public static void main(String[] args){
+		String numberinput = JOptionPane.showInputDialog("Escreva um número!");
+		int number = Integer.parseInt(numberinput);
+
+		String numberSignal = CheckNumberSignal(number);
+
+		JOptionPane.showMessageDialog(null, "O seu número: " +number+ " é " + numberSignal);
+
+
+}
+	public static String CheckNumberSignal (int number)
+	{
+		if (number > 0){
+			return "positivo";
+		}
+		else if (number < 0)
+		{
+			return "negativo";
+		}
+		else {
+			return "zero";
+		}
+	}
+
+}
